a9692c6fba744615db5c7b91352259af6d8b6265
Veerendra Patel <veerendra.patel@etouches.com>
2020-05-07 23:45:28 +0530

    handle side nav

--- marioplan/src/components/layout/SignedInLinks.js
+++ marioplan/src/components/layout/SignedInLinks.js
@@ -8,13 +8,16 @@ const SignedInLinks = (props) => {
   // if(props.profile.firstName && props.profile.lastName) {
   //   initials = props.profile.firstName.charAt(0) + props.profile.lastName.charAt(0)
   // }
+  const handleClick = () => {
+    window.location.reload(false);
+  }
   return (
     <ul>
-      <li><NavLink to='/create'><i className="small material-icons">add</i>Add Member</NavLink></li> 
-      <li><a onClick={props.signOut}><i className="small material-icons">exit_to_app</i>Log Out</a></li>
-      {/* <li><NavLink to='/' className="btn btn-floating pink lighten-1">
-          {initials}
-      </NavLink></li> */}
+      <li onClick={handleClick}><NavLink to='/'><i className="small material-icons">home</i>
+          Home
+      </NavLink></li>
+      <li onClick={handleClick}><NavLink to='/create'><i className="small material-icons">add</i>Add Member</NavLink></li> 
+      <li onClick={handleClick}><a onClick={props.signOut}><i className="small material-icons">exit_to_app</i>Log Out</a></li>
     </ul>
   )
 }
--- marioplan/src/components/layout/SignedOutLinks.js
+++ marioplan/src/components/layout/SignedOutLinks.js
@@ -2,10 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const SignedOutLinks = () => {
+  const handleClick = () => {
+    window.location.reload(false);
+  }
   return (
       <ul>
-        <li><NavLink to='/signup'><i className="small material-icons">filter_tilt_shift</i>Signup</NavLink></li>
-        <li><NavLink to='/signin'><i className="small material-icons">event_seat</i>Login</NavLink></li>
+        <li onClick={handleClick}><NavLink to='/signup'><i className="small material-icons">filter_tilt_shift</i>Signup</NavLink></li>
+        <li onClick={handleClick}><NavLink to='/signin'><i className="small material-icons">event_seat</i>Login</NavLink></li>
       </ul>
   )
 }
