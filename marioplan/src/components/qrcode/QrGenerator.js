334e764a17e354f1220a72828235e3ef662faff3
Veerendra Patel <veerendra.patel@etouches.com>
2020-05-21 22:12:23 +0530

    add share button

--- marioplan/src/components/qrcode/QrGenerator.js
+++ marioplan/src/components/qrcode/QrGenerator.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import QrCode  from 'react.qrcode.generator'
+import {
+  WhatsappShareButton,
+  WhatsappIcon
+} from "react-share";
 class QrGenerator extends Component {
   render() {
     const { qrValues } = this.props;
@@ -12,6 +16,8 @@ class QrGenerator extends Component {
       <div className="card-image">
       <QrCode value={qrValues} size='200'/>
       </div>
+      
+      <WhatsappShareButton children="<QrCode value={qrValues} size='200'/>" url = "http://localhost:3000/qrcode/visitor" title="hello"><WhatsappIcon size={32} round={true} /></WhatsappShareButton>
           <div className="card-content">
             <p> Please share this with visitor. Security man will scan this and let the visitor in
             </p>
