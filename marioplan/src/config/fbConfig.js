9eb1c3befebaa9da8977c8389682b7b8d50978f1
veerendrapatel <veerendra.patel@gmail.com>
2020-05-21 22:27:06 +0530

    Update fbConfig.js

--- marioplan/src/config/fbConfig.js
+++ marioplan/src/config/fbConfig.js
@@ -4,14 +4,14 @@ import 'firebase/auth';
 
 // Replace this with your own config details
 var firebaseConfig = {
-  apiKey: "AIzaSyBX_WvrPYpM2-zgtY5tgWvQPs5AG1DUgT4",
-  authDomain: "simandhar-7b623.firebaseapp.com",
-  databaseURL: "https://simandhar-7b623.firebaseio.com",
-  projectId: "simandhar-7b623",
-  storageBucket: "simandhar-7b623.appspot.com",
-  messagingSenderId: "686820060754",
-  appId: "1:686820060754:web:ea7373e3b57226148c798b",
-  measurementId: "G-60XYCT4XER"
+  apiKey: "XXX",
+  authDomain: "XXX",
+  databaseURL: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX",
+  measurementId: "XXX"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
